6543cfc95ee07e6f8dca7149d0740dad16586c9b
sanjeetkushwaha <sanjeet@vidyakul.com>
2024-10-07 02:10:14 +0530

    code refactored

--- androidassignment/src/main/java/com/sanjeet/androidassignment/ui/characterlist/CharacterListActivity.kt
+++ androidassignment/src/main/java/com/sanjeet/androidassignment/ui/characterlist/CharacterListActivity.kt
@@ -1,8 +1,8 @@
 package com.sanjeet.androidassignment.ui.characterlist
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sanjeet.androidassignment.MyApplication
 import com.sanjeet.androidassignment.api.RetrofitHelper
@@ -10,43 +10,39 @@ import com.sanjeet.androidassignment.data.characterModel.CharacterList
 import com.sanjeet.androidassignment.data.repository.CharacterRepository
 import com.sanjeet.androidassignment.databinding.ActivityCharacterListBinding
 
-
 class CharacterListActivity : AppCompatActivity() {
 
-
-    private val bookMarDao by lazy {
-        (application as MyApplication).database.bookMarkDao()
-    }
-
-    private val viewModel by lazy {
-        ViewModelProvider(
-            this,
-            CharacterViewModelFactory(CharacterRepository(RetrofitHelper.apiService, bookMarDao))
-        )[CharachterViewModel::class.java]
-    }
     private lateinit var binding: ActivityCharacterListBinding
-    private val characterListAdapter by lazy {
-        CharacterListAdapter(arrayListOf()) { characterList, isStarred ->
-            starClicked(characterList, isStarred)
-        }
+    private val viewModel: CharacterViewModel by viewModels {
+        CharacterViewModelFactory(CharacterRepository(RetrofitHelper.apiService, (application as MyApplication).database.bookMarkDao()))
+    }
+    private val characterListAdapter = CharacterListAdapter(arrayListOf()) { characterList, isStarred ->
+        starClicked(characterList, isStarred)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCharacterListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+        observeViewModel()
+        viewModel.fetchCharacterList()
+    }
+
+    private fun setupRecyclerView() {
         binding.listRv.apply {
             adapter = characterListAdapter
             layoutManager = LinearLayoutManager(this@CharacterListActivity)
         }
-        viewModel.getCharacterList()
+    }
+
+    private fun observeViewModel() {
         viewModel.characterLiveData.observe(this) { characters ->
             characterListAdapter.updateAdapter(characters)
         }
     }
 
-
     private fun starClicked(characterList: CharacterList, isBookMarked: Boolean) {
-        viewModel.bookMarkCharacter(characterList, isBookMarked)
+        viewModel.toggleBookmarkForCharacter(characterList, isBookMarked)
     }
 }
