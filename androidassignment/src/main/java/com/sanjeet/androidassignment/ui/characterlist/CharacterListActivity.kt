33b3d33d6a6c16bf450a91d524b40afd71f2befd
sanjeetkushwaha <sanjeet@vidyakul.com>
2024-10-07 01:50:17 +0530

    bookmark enable with room storage

--- androidassignment/src/main/java/com/sanjeet/androidassignment/ui/characterlist/CharacterListActivity.kt
+++ androidassignment/src/main/java/com/sanjeet/androidassignment/ui/characterlist/CharacterListActivity.kt
@@ -4,20 +4,32 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sanjeet.androidassignment.MyApplication
 import com.sanjeet.androidassignment.api.RetrofitHelper
+import com.sanjeet.androidassignment.data.characterModel.CharacterList
 import com.sanjeet.androidassignment.data.repository.CharacterRepository
 import com.sanjeet.androidassignment.databinding.ActivityCharacterListBinding
 
+
 class CharacterListActivity : AppCompatActivity() {
+
+
+    private val bookMarDao by lazy {
+        (application as MyApplication).database.bookMarkDao()
+    }
 
     private val viewModel by lazy {
         ViewModelProvider(
             this,
-            CharacterViewModelFactory(CharacterRepository(RetrofitHelper.apiService))
+            CharacterViewModelFactory(CharacterRepository(RetrofitHelper.apiService, bookMarDao))
         )[CharachterViewModel::class.java]
     }
     private lateinit var binding: ActivityCharacterListBinding
-    private val characterListAdapter by lazy { CharacterListAdapter(arrayListOf()) }
+    private val characterListAdapter by lazy {
+        CharacterListAdapter(arrayListOf()) { characterList, isStarred ->
+            starClicked(characterList, isStarred)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +40,13 @@ class CharacterListActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@CharacterListActivity)
         }
         viewModel.getCharacterList()
-
         viewModel.characterLiveData.observe(this) { characters ->
             characterListAdapter.updateAdapter(characters)
         }
+    }
+
+
+    private fun starClicked(characterList: CharacterList, isBookMarked: Boolean) {
+        viewModel.bookMarkCharacter(characterList, isBookMarked)
     }
 }
